5facf11a3949f90032e041c32e6016d933bd52f2
sushri <sahoosushrib@gmail.com>
2020-08-18 19:46:55 +0530

    first commit

---
+++ src/test/java/healthcare/mavenprojects/AppiumTest.java
@@ -0,0 +1,19 @@
+package healthcare.mavenprojects;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAPAndroid() {
+		System.out.println("NativeAPAndroid");
+		
+	}
+	
+	@Test
+	public void IOSApps() {
+		System.out.println("IOSApps");
+		
+	}
+
+}
---
+++ src/test/java/healthcare/mavenprojects/RESTApiTest.java
@@ -0,0 +1,17 @@
+package healthcare.mavenprojects;
+
+import org.testng.annotations.Test;
+
+public class RESTApiTest {
+	
+	@Test
+	public void Postjira () {
+		System.out.println("Postjira");
+	}
+	
+	@Test
+	public void deletetwiter () {
+		System.out.println("deletetwiter");
+	}
+
+}
